982676a0fcf2c2471625e166b433bd650bbb8612
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-05 15:58:45 +0200

    first commit

---
+++ inc/Movie.php
@@ -0,0 +1,74 @@
+<?php
+
+ class Movie{
+   public $id;    //?private attributes
+   public $title; //?private attributes
+   public $genre; //? Enum class
+   public $lang;  //? Enum class
+   public $vote;
+   public $poster_path;
+
+/**
+ * Movie Contructor 
+ * @param integer $_id
+ * @param string $_title
+ * @param string $_genre
+ * @param string $_lang
+ * @param integer $_vote
+ * @param string $_poster_path
+ */
+ public function __construct($_id,$_title,$_genre='action',$_lang='it',$_vote=1,$_poster_path='unkonwn'){
+   $this->id=$_id;
+   $this->title=$_title;
+   $this->genre=$_genre;
+   $this->lang=$_lang;
+   $this->vote=$_vote;
+   $this->poster_path=$_poster_path;
+ }
+
+ //?GETTER 
+ /**
+  * @return string title
+  */
+ public function getTitle(){
+     return $this->title;
+ }
+ /**
+  * @return string genre
+  */
+ public function getGenre(){
+     return $this->genre;
+ }
+ /**
+  * @return string lang
+  */
+ public function getLang(){
+     return $this->lang;
+ }
+ /**
+  * @return integer vote
+  */
+ public function getVote(){
+     return $this->vote;
+ }
+ /**
+  * @return string poster url
+  */
+ public function getPosterPath(){
+     return $this->poster_path;
+ }
+
+ public function  render()
+ {
+     return '<div class="movie">
+     <h1>Titolo:'. $this->getTitle() .'</h1>
+     <p>Genre: '. $this->getGenre() .'</br> Lingua:  '. $this->getLang() .'</p>
+     <small>Voto: '. $this->getVote() .'/10 </small>
+   </div>';
+ }
+}
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . '/inc/Movie.php';
+/** 
+ *! - è definita una classe ‘Movie’
+ *  ? => all'interno della classe sono dichiarate delle variabili
+ *  ?    d'istanza (proprietà)
+ *  ? => all'interno della classe è definito un costruttore
+ *  ? => all'interno della classe è definito almeno un metodo
+ *!- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i 
+ *  * valori delle relative proprietà, se possibile cerchiamo di stampare senza 
+ *  *  ricorrere a var_dump
+ * 
+ */
+ 
+
+
+$m1=new Movie(872200,'Ritorno al futuro - 30 anni dopo','fantasy','it',1,'ciao.png');
+$m2=new Movie(872200,'Guardiani della galassia ','fantasy','en',3,'bye.png');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP1: class</title>
+</head>
+<body>
+      <?php echo $m1->render()?>
+      <?php echo $m2->render()?>
+</body>
+</html>
